e4bff011768d256a7a210da3ca9a2cfc14b330ff
Steve Smith <steve@kentsmiths.com>
2020-05-19 09:26:32 -0400

    remove about link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -64,9 +64,6 @@ const Navbar = class extends React.Component {
             className={`navbar-menu ${this.state.navBarActiveClass}`}
           >
             <div className="navbar-start has-text-centered">
-              <Link className="navbar-item" to="/">
-                About
-              </Link>
               <Link className="navbar-item" to="/blog">
                 Blog
               </Link>

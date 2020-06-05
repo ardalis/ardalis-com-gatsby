18af12eef3ce9c72f4edc03b10dc3ff943ecae06
Steve Smith <steve@kentsmiths.com>
2020-06-05 16:23:31 -0400

    configuring google tag manager

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,13 @@ module.exports = {
       },
   },
   plugins: [
+    {
+      resolve: "gatsby-plugin-google-tagmanager",
+      options: {
+        id: "GTM-NVBMVX8",
+        includeInDevelopment: false,
+      }
+    },
     'gatsby-plugin-react-helmet',
     `gatsby-plugin-twitter`,
     'gatsby-plugin-sass',
@@ -146,31 +153,6 @@ module.exports = {
       resolve: 'gatsby-plugin-netlify-cms',
       options: {
         modulePath: `${__dirname}/src/cms/cms.js`,
-      },
-    },
-    {
-      resolve: `gatsby-plugin-google-gtag`,
-      options: {
-        // You can add multiple tracking ids and a pageview event will be fired for all of them.
-        trackingIds: [
-          "GA-UA-470225-25", // Google Analytics / GA
-        ],
-        // This object gets passed directly to the gtag config command
-        // This config will be shared across all trackingIds
-        gtagConfig: {
-          anonymize_ip: false,
-          cookie_expires: 0,
-          send_page_view: true
-        },
-        // This object is used for configuration specific to this plugin
-        pluginConfig: {
-          // Puts tracking script in the head instead of the body
-          head: true,
-          // Setting this parameter is also optional
-          respectDNT: true,
-          // Avoids sending pageview hits from custom paths
-          exclude: ["/preview/**", "/do-not-track/me/too/"],
-        },
       },
     },
     {

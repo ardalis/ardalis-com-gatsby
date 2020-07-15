eb8a8d7d1b30a66e2ed452a972ae73e8b2323f0f
Ilyana Smith <ilyana@kentsmiths.com>
2020-07-15 15:16:34 -0400

    rss limited to 10

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,7 +36,6 @@ module.exports = {
     'gatsby-plugin-sass',
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
-    'gatsby-plugin-feed',
     {
       // keep as first gatsby-source-filesystem plugin for gatsby image support
       resolve: 'gatsby-source-filesystem',
@@ -141,6 +140,66 @@ module.exports = {
               // Add additional HTML escapes by providing a mapping
               // of HTML entities and their escape value IE: { '}': '&#123;' }
               escapeEntities: {},
+            },
+          },
+          {
+            resolve: `gatsby-plugin-feed`,
+            options: {
+              query: `
+                {
+                  site {
+                    siteMetadata {
+                      title
+                      description
+                      siteUrl
+                      site_url: siteUrl
+                    }
+                  }
+                }
+              `,
+              feeds: [
+                {
+                  serialize: ({ query: { site, allMarkdownRemark } }) => {
+                    return allMarkdownRemark.edges.map(edge => {
+                      return Object.assign({}, edge.node.frontmatter, {
+                        description: edge.node.excerpt,
+                        date: edge.node.frontmatter.date,
+                        url: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                        guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                        custom_elements: [{ "content:encoded": edge.node.html }],
+                      })
+                    })
+                  },
+                  query: `
+                    {
+                      allMarkdownRemark(
+                        sort: { order: DESC, fields: [frontmatter___date] },
+                        filter: {fileAbsolutePath: {regex: "content/blog/"}},
+                        limit: 10
+                      ) {
+                        edges {
+                          node {
+                            excerpt
+                            html
+                            fields { slug }
+                            frontmatter {
+                              title
+                              date
+                            }
+                          }
+                        }
+                      }
+                    }
+                  `,
+                  output: "/rss.xml",
+                  title: "Your Site's RSS Feed",
+                  // optional configuration to insert feed reference in pages:
+                  // if `string` is used, it will be used to create RegExp and then test if pathname of
+                  // current page satisfied this regular expression;
+                  // if not provided or `undefined`, all pages will have feed reference inserted
+                  match: "^/blog/",
+                },
+              ],
             },
           },
           {

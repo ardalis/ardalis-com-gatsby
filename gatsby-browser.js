6d2dbb12bd1b0a1e022bedcbf6bb2b7c80520a4e
Steve Smith <steve@kentsmiths.com>
2024-04-01 16:54:19 -0400

    add logging in onRouteUpdate

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,9 @@
 require("prismjs/plugins/line-numbers/prism-line-numbers.css")
 require("prismjs/plugins/command-line/prism-command-line.css")
 require("prismjs/themes/prism-okaidia.css")
+
+exports.onRouteUpdate = () => {
+    // Code to reload your ad script
+    console.log('Page navigation occurred, reloading ad script...');
+    // Example: reloadAdScript();
+};
